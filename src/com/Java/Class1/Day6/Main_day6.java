1662601fa3aa9b34072be85491c44f765673828e
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-10 16:31:41 +0900

    Abstract class example

--- src/com/Java/Class1/Day6/Main_day6.java
+++ src/com/Java/Class1/Day6/Main_day6.java
@@ -7,20 +7,29 @@ public class Main_day6  {
         projects IT1=new ITProjects();
         Business1.about();
         IT1.about();
+        IT1.getName();
     }
 }
 
 abstract class projects{
     abstract void about();
+    abstract void getName();
 
 }
 
 class BusinessProjects extends projects{
+
+    void getName(){
+        System.out.println("Business Project Name");
+    }
     void about(){
         System.out.println("this is about Business projects");
     }
 }
 class ITProjects extends projects{
+    void getName(){
+        System.out.println("IT project name");
+    }
     void about(){
         System.out.println("this is about IT projects");
     }
---
+++ src/com/Java/Class1/Day6/Main_day6_Interface.java
@@ -0,0 +1,8 @@
+package com.Java.Class1.Day6;
+
+public class Main_day6_Interface {
+    public static void main(String[] args) {
+
+    }
+}
+
