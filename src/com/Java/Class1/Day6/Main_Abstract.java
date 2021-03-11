400eb90b3afc308882ce8365113cb8d4c110e20d
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-11 16:55:10 +0900

    Abstract Class example

---
+++ src/com/Java/Class1/Day6/Main_Abstract.java
@@ -0,0 +1,9 @@
+package com.Java.Class1.Day6;
+
+public class Main_Abstract {
+    public static void main(String[] args) {
+
+        Animal MyDog = new Dog("Tommy");
+        MyDog.eat();
+    }
+}
