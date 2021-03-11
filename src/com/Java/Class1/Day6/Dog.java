b54a73ac312bc825d484d694ce398302d6984173
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-11 17:03:07 +0900

    Abstract Class example

--- src/com/Java/Class1/Day6/Dog.java
+++ src/com/Java/Class1/Day6/Dog.java
@@ -1,12 +1,35 @@
 package com.Java.Class1.Day6;
 
-public class Dog extends Animal{
+public abstract class Dog extends Animal{
     Dog(String name){
         super(name);
     }
     @Override
     public void eat() {
         System.out.println(getName()+" eats bone");
+    }
+    public abstract void Hunt();
+
+}
+
+class pugDog extends Dog{
+
+pugDog(String name){
+    super(name);
+
+}
+    @Override
+    public void Hunt() {
+        System.out.println(getName()+"- I am cute and i cannot hunt");
+    }
+}
+class RajapalayamDog extends Dog{
+RajapalayamDog(String name){
+    super(name);
+}
+    @Override
+    public void Hunt() {
+        System.out.println(getName()+"- I am cute , But i can Hunt");
     }
 
 }
--- src/com/Java/Class1/Day6/Main_Abstract.java
+++ src/com/Java/Class1/Day6/Main_Abstract.java
@@ -3,7 +3,13 @@ package com.Java.Class1.Day6;
 public class Main_Abstract {
     public static void main(String[] args) {
 
-        Animal MyDog = new Dog("Tommy");
+        Dog MyDog = new pugDog("Tommy");
         MyDog.eat();
+        MyDog.Hunt();
+
+        Dog SomeOneDog=new RajapalayamDog("Huntter");
+        SomeOneDog.eat();
+        SomeOneDog.Hunt();
+
     }
 }
