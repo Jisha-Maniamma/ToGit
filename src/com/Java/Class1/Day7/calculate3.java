d642ace37eaa1dc6e18023a6bf484fab3eb1f9d1
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-17 13:21:41 +0900

    Dates Calculation

--- src/com/Java/Class1/Day7/calculate3.java
+++ src/com/Java/Class1/Day7/calculate3.java
@@ -1,4 +1,34 @@
 package com.Java.Class1.Day7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class calculate3 {
+    List<Person> p1=new ArrayList<>();
+    p1.add();
+}
+class Person{
+    private int Age;
+    private String name;
+
+    public Person(int age, String name) {
+        Age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
