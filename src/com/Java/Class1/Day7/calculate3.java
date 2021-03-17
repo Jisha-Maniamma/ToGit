b529dc429f0c5b0318c9f742be3c25d0383c1611
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-17 13:31:02 +0900

    List to observable list

--- src/com/Java/Class1/Day7/calculate3.java
+++ src/com/Java/Class1/Day7/calculate3.java
@@ -1,11 +1,21 @@
 package com.Java.Class1.Day7;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class calculate3 {
-    List<Person> p1=new ArrayList<>();
-    p1.add();
+    public static void main(String[] args) {
+
+        List<Person> p1=new ArrayList<Person>();
+        p1.add(new Person(4,"jisha"));
+        p1.add(new Person(5,"krishna"));
+
+        ObservableList<Person> p= FXCollections.observableArrayList(p1);
+        System.out.println(p.get(0).getName()+" "+p.get(1).getName());
+    }
 }
 class Person{
     private int Age;
