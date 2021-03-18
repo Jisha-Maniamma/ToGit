7794bdf106896454a921d204d9096ebbcdd1069b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-18 14:40:09 +0900

    To understand Singelton class

--- src/com/Java/Class1/Day11/Data/Main.java
+++ src/com/Java/Class1/Day11/Data/Main.java
@@ -1,4 +1,10 @@
 package com.Java.Class1.Day11.Data;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -6,6 +12,20 @@ package com.Java.Class1.Day11.Data;
  */
 public class Main {
     public static void main(String[] args) {
+
+            ToDoItems item1 = new ToDoItems("Mail birthday card", "John",
+                    LocalDate.of(2016, Month.APRIL, 25));
+            ToDoItems item2 = new ToDoItems("Doctor's Appointment", "Jisha",
+                    LocalDate.of(2016, Month.MAY, 23));
+            ToDoItems item3 = new ToDoItems("Finish design proposal for client", "James",
+                    LocalDate.of(2016, Month.APRIL, 22));
+            ToDoItems item4 = new ToDoItems("Pickup Doug at the train station", "Jacob",
+                    LocalDate.of(2016, Month.MARCH, 23));
+            ToDoItems item5 = new ToDoItems("Pick up dry cleaning", "Ron",
+                    LocalDate.of(2016, Month.APRIL,20));
+
+        List<ToDoItems> List=new ArrayList<ToDoItems>();
+
 
     }
 }
--- src/com/Java/Class1/Day11/Data/ToDoData.java
+++ src/com/Java/Class1/Day11/Data/ToDoData.java
@@ -1,24 +1,30 @@
 package com.Java.Class1.Day11.Data;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
 import java.util.List;
 
 public class ToDoData {
-private static ToDoData instance=new ToDoData();
-private static String FILE_NAME="TODO_Details.txt";
+    private static ToDoData instance=new ToDoData();
+    private static String FILE_NAME="TODO_Details.txt";
 
-private List<ToDoItems> todoItems;
-private DateTimeFormatter df;
+    private List<ToDoItems> todoItems;
+    private DateTimeFormatter df;
 
 
-public static  ToDoData getInstance(){
-    return instance;
-}
+    public static  ToDoData getInstance(){
+        return instance;
+    }
 
-private  ToDoData(){
-    df=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private  ToDoData(){
+        df=DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
-}
+    }
 
     public List<ToDoItems> getTodoItems() {
         return todoItems;
@@ -31,9 +37,30 @@ private  ToDoData(){
     ///to load text
     public void LoadFromText(){
 
+
     }
     //to save to text
-    public void storeToText(){
+    public void storeToText() throws IOException {
+        Path path= Paths.get(FILE_NAME);
+        BufferedWriter bw= Files.newBufferedWriter(path);
+        try{
+            Iterator<ToDoItems> iter=todoItems.iterator();
+                    while(iter.hasNext())
+                    {
+                        ToDoItems items=iter.next();
+                        bw.write(String.format("%s, %s, %s", items.getPersonName()
+                        , items.getTaskName(), items.getDateOfTask().format(df)));
+                        bw.newLine();
+                    }
+        }finally{
+            if(bw!=null){
+                bw.close();
+            }
+        }
+//        catch( Exception e){
+//            System.out.println(e.getMessage());
+//        }
+
 
     }
 
