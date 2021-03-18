41d2696d4024d5c23a73c9902d8034a01bc6b1c9
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-18 15:05:04 +0900

    To understand Singelton class

--- src/com/Java/Class1/Day11/Data/Main.java
+++ src/com/Java/Class1/Day11/Data/Main.java
@@ -1,10 +1,5 @@
 package com.Java.Class1.Day11.Data;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -12,27 +7,34 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-//stoop();
-        ToDoItems item1 = new ToDoItems("Mail birthday card", "John",
-                LocalDate.of(2016, Month.APRIL, 25));
-        ToDoItems item2 = new ToDoItems("Doctor's Appointment", "Jisha",
-                LocalDate.of(2016, Month.MAY, 23));
-        ToDoItems item3 = new ToDoItems("Finish design proposal for client", "James",
-                LocalDate.of(2016, Month.APRIL, 22));
-        ToDoItems item4 = new ToDoItems("Pickup Doug at the train station", "Jacob",
-                LocalDate.of(2016, Month.MARCH, 23));
-        ToDoItems item5 = new ToDoItems("Pick up dry cleaning", "Ron",
-                LocalDate.of(2016, Month.APRIL,20));
 
-        List<ToDoItems> toDoItems=new ArrayList<ToDoItems>();
-        toDoItems.add(item1);
-        toDoItems.add(item2);
-        toDoItems.add(item3);
-        toDoItems.add(item4);
-        toDoItems.add(item5);
-        ToDoData.getInstance().setTodoItems(toDoItems);
+        /**
+         * Only required first time then not required.
+         *
+         * @return  the details to be stored in the text format {@code Main} instance
+         *
+         */
 
-        stoop();
+//        ToDoItems item1 = new ToDoItems("Mail birthday card", "John",
+//                LocalDate.of(2016, Month.APRIL, 25));
+//        ToDoItems item2 = new ToDoItems("Doctor's Appointment", "Jisha",
+//                LocalDate.of(2016, Month.MAY, 23));
+//        ToDoItems item3 = new ToDoItems("Finish design proposal for client", "James",
+//                LocalDate.of(2016, Month.APRIL, 22));
+//        ToDoItems item4 = new ToDoItems("Pickup Doug at the train station", "Jacob",
+//                LocalDate.of(2016, Month.MARCH, 23));
+//        ToDoItems item5 = new ToDoItems("Pick up dry cleaning", "Ron",
+//                LocalDate.of(2016, Month.APRIL,20));
+//
+//        List<ToDoItems> toDoItems=new ArrayList<ToDoItems>();
+//        toDoItems.add(item1);
+//        toDoItems.add(item2);
+//        toDoItems.add(item3);
+//        toDoItems.add(item4);
+//        toDoItems.add(item5);
+//        ToDoData.getInstance().setTodoItems(toDoItems);
+//
+//        stoop();
     }
 
     private static void stoop() {
