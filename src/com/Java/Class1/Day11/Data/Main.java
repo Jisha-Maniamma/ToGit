3d54466735ddd698a7e5cc49057eb92870005a71
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-18 15:08:52 +0900

    To understand Singelton class

--- src/com/Java/Class1/Day11/Data/Main.java
+++ src/com/Java/Class1/Day11/Data/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
 /**
  * Only required first time then not required.
- *
  * @return  the details to be stored in the text format {@code Main} instance
  *
  */
