742fad467b8ea5a773324f4a4f2f9a9030278b8c
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-19 16:47:17 +0900

    Java

--- src/com/Java/Class1/Day7/CalculateStartDate_v1.java
+++ src/com/Java/Class1/Day7/CalculateStartDate_v1.java
@@ -2,6 +2,8 @@ package com.Java.Class1.Day7;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jisha Maniamma
@@ -10,28 +12,72 @@ import java.time.LocalDate;
  */
 public class CalculateStartDate_v1 {
     public static void main(String[] args) {
-        calcul(LocalDate.of(2021,03,22),5);
+        AddHolidays(LocalDate.of(2020,01,01) ,
+                LocalDate.of(2020,01,02)
+                ,LocalDate.of(2020,01,03)
+                ,LocalDate.of(2020,05,04)
+                ,LocalDate.of(2020,05,05)
+                ,LocalDate.of(2020,05,06)
+                ,LocalDate.of(2020,05,07)
+                ,LocalDate.of(2020,05,8)
+                ,LocalDate.of(2021,03,17)
+                ,LocalDate.of(2021,03,19)
+                ,LocalDate.of(2020,8,10)
+                ,LocalDate.of(2020,8,11)
+                ,LocalDate.of(2020,8,12)
+                ,LocalDate.of(2020,8,13)
+                ,LocalDate.of(2020,8,14)
+                ,LocalDate.of(2020,9,21)
+                ,LocalDate.of(2020,12,30)
+                ,LocalDate.of(2020,12,31)
+
+        );
+        calcul(LocalDate.of(2021,04,19),20);
         //  calcul1(LocalDate.of(2021,04,17),34);
     }
 
-    private static void calcul(LocalDate Enddate, int i) {
+
+    public static List<LocalDate> dates=new ArrayList<>();
+
+
+
+
+
+    public static List<LocalDate> AddHolidays(LocalDate... datesInput){
+
+        for(LocalDate NewDates:datesInput){
+
+            dates.add(NewDates);
+        }
+        return dates;
+    }
+
+    public static List<LocalDate> AddDates(LocalDate date1){
+
+        dates.add(date1);
+        return dates;
+    }
+
+
+    private static void calcul(LocalDate Enddate, double i) {
         LocalDate end=Enddate;
-        int days=i;
-        System.out.println("...To reduce no. of days: "+days+" From date: "+end+"...");
-        int ii=0;
+        double days=i;
+        System.out.println("...To reduce.... no. of Milestone date: "+days+"..... From date: "+end+"...");
+        System.out.println("Number of holidays "+dates.size());int ii=0;
         for(;ii<=days;){
 
-            if(end.getDayOfWeek()== DayOfWeek.SATURDAY || end.getDayOfWeek()==DayOfWeek.SUNDAY ){
-                System.out.println("Weekend");end=end.minusDays(1);
+            if(dates.contains(end) ||end.getDayOfWeek()== DayOfWeek.SATURDAY || end.getDayOfWeek()==DayOfWeek.SUNDAY ){
+               // System.out.println("Weekend "+end);
+                end=end.minusDays(1);
 
             }
             else{
-                System.out.println("not weekend " +end);
+               // System.out.println("not weekend " +end);
                 end=end.minusDays(1);
                 ii++;
             }
         }
-        System.out.println(end.plusDays(1));
+        System.out.println("Scheduld start date is "+end.plusDays(1));
 
     }
 }
