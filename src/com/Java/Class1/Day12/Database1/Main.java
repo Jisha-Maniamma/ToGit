37d1a11d721774d51f91ca639c6a3fea5860b7d0
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-22 15:21:38 +0900

    Starting Database with sqlLite

--- src/com/Java/Class1/Day12/Database1/Main.java
+++ src/com/Java/Class1/Day12/Database1/Main.java
@@ -7,10 +7,19 @@ package com.Java.Class1.Day12.Database1;
  */
 public class Main {
     public static void main(String[] args) {
+
+
 //SELECT songs.track,songs.album,albums.name FROM songs
 // JOIN albums ON songs.album=albums._id
 // ORDER by albums.name,songs.track COLLATE NOCASE DESC;
 
+
+
+
+//SELECT artists.name,albums.name,songs.track,songs.title FROM songs
+//INNER JOIN albums on songs.album=albums._id
+//INNER JOIN artists on albums.artist=artists._id
+//ORDER BY artists.name;
 
     }
 }
