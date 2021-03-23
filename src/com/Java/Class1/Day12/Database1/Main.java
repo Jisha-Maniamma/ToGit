b01de0a9a4437517c263423f7e56104a37b64ef1
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-23 11:42:17 +0900

    working with JDBC

--- src/com/Java/Class1/Day12/Database1/Main.java
+++ src/com/Java/Class1/Day12/Database1/Main.java
@@ -14,19 +14,29 @@ public class Main {
         try{
             Connection conn= DriverManager.getConnection("jdbc:sqlite:sample.db");
 
-           // conn.setAutoCommit(false);
+            // conn.setAutoCommit(false);
             Statement statement=conn.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS company( _id INTEGER, name TEXT,departmentName TEXT)");
             statement.execute("INSERT into company(_id,name,departmentName) values(7,'Juntos Consulting','Customer Care')");
             statement.execute("INSERT into company(_id,name,departmentName) values(6,'Juntos Engineering','civil')");
-           // statement.execute("update company SET _id=5 where name='Juntos Engineering'");
-           // statement.execute("delete from company where name='Juntos Engineering'");
+            // statement.execute("update company SET _id=5 where name='Juntos Engineering'");
+            // statement.execute("delete from company where name='Juntos Engineering'");
 
-           statement.execute("select DISTINCT name  from company");
+            statement.execute("select DISTINCT name, _id from company");
             ResultSet result=statement.getResultSet();
             while (result.next()){
-                System.out.println("name : "+result.getString("name"));
+                System.out.println("name : "+result.getString("name")+
+                        " _id: "+result.getInt("_id"));
             }
+            /*
+            Juntos Consulting	1
+            Juntos Consulting	3
+            Juntos Engineering	4
+            Juntos Consulting	5
+            Juntos Engineering	6
+            Juntos Consulting	7
+             */
+            result.close();
             statement.close();
             conn.close();
         }catch (SQLException e){
