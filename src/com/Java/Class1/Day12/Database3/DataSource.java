f1cd80c2d86a9880f5e761d77ed5059bcd783154
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-23 15:25:19 +0900

    working with JDBC

--- src/com/Java/Class1/Day12/Database3/DataSource.java
+++ src/com/Java/Class1/Day12/Database3/DataSource.java
@@ -6,7 +6,17 @@ package com.Java.Class1.Day12.Database3;
  * @date 2021/03/23 15:09
  */
 public class DataSource {
-public static final String DB_NAME="";
+    public static final String DB_NAME="music.db";
+    public static final String CONNECTION="jdbc:sqlite:"+DB_NAME;
+
+    public static final String TABLE_ALBUM="albums";
+    public static final String TABLE_ALBUM_ID="_id";
+    public static final String TABLE_ALBUM_NAME="name";
+    public static final String TABLE_ALBUM_ARTIST="artist";
+
+
+
+
     public static void main(String[] args) {
 
     }
