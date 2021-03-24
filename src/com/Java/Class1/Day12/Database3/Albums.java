df50cc7577a684ecf3f673f5bfeedcfb2faa8bf7
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-24 09:22:00 +0900

    Sqlite example

---
+++ src/com/Java/Class1/Day12/Database3/Albums.java
@@ -0,0 +1,37 @@
+package com.Java.Class1.Day12.Database3;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/03/24 9:20
+ */
+public class Albums {
+
+    private int id;
+    private String name;
+    private int artistID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArtistID() {
+        return artistID;
+    }
+
+    public void setArtistID(int artistID) {
+        this.artistID = artistID;
+    }
+}
