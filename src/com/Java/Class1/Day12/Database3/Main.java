95e28b9e46bf8f2aa0a1bb3e392c48131fc30556
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-24 10:00:29 +0900

    Sqlite example

--- src/com/Java/Class1/Day12/Database3/Main.java
+++ src/com/Java/Class1/Day12/Database3/Main.java
@@ -1,5 +1,7 @@
 package com.Java.Class1.Day12.Database3;
 
+import java.util.List;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -13,6 +15,16 @@ public class Main {
             System.out.println("cant open");
             return;
         }
+
+        List<Artists> artists=dataSource.querryArtist();
+        if(artists==null){
+            System.out.println("Empty artist data");
+            return;
+        }
+
+       for(Artists a:artists){
+           System.out.println("The name  of the artist is- "+a.getName());
+       }
         dataSource.close();
     }
 }
