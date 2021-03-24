b510f861d3b462bd2eee63db3b89a788f7d08778
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-24 09:19:48 +0900

    Sqlite example

---
+++ src/com/Java/Class1/Day12/Database3/Artists.java
@@ -0,0 +1,27 @@
+package com.Java.Class1.Day12.Database3;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/03/24 9:19
+ */
+public class Artists {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
