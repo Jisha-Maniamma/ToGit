18b052ae8edfb1392e22c8812e9b52c4e009910b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-25 09:55:38 +0900

    Working with JDBC

--- src/com/Java/Class1/Day12/Database3/SongArtistAlbum.java
+++ src/com/Java/Class1/Day12/Database3/SongArtistAlbum.java
@@ -5,7 +5,8 @@ package com.Java.Class1.Day12.Database3;
  * @version 1.0
  * @date 2021/03/25 9:49
  *
- * Using this call
+ * Using this class- query the song ...
+ * to get its album name (album table), artist name (artist table) and track number (Songs table)
  */
 public class SongArtistAlbum {
 }
