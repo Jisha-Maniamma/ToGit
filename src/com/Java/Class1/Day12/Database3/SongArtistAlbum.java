4a85402e1ab1c14797f4d6449bd18ad25d493dc1
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-25 09:57:14 +0900

    Working with JDBC

--- src/com/Java/Class1/Day12/Database3/SongArtistAlbum.java
+++ src/com/Java/Class1/Day12/Database3/SongArtistAlbum.java
@@ -9,4 +9,31 @@ package com.Java.Class1.Day12.Database3;
  * to get its album name (album table), artist name (artist table) and track number (Songs table)
  */
 public class SongArtistAlbum {
+    private String artistName;
+    private String albumName;
+    private int songTrackNumber;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public int getSongTrackNumber() {
+        return songTrackNumber;
+    }
+
+    public void setSongTrackNumber(int songTrackNumber) {
+        this.songTrackNumber = songTrackNumber;
+    }
 }
