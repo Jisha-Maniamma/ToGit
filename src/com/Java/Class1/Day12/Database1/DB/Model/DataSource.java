db7565c83b58337f40ce03e1a39fcaa4bc65eab7
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-26 10:50:18 +0900

    DataBase creadtion

--- src/com/Java/Class1/Day12/Database1/DB/Model/DataSource.java
+++ src/com/Java/Class1/Day12/Database1/DB/Model/DataSource.java
@@ -13,7 +13,7 @@ import java.sql.Statement;
 public class DataSource {
 
     public static final String DATABASE_NAME="Master_Project_Info.db";
-    public static final String CONNECTION="jdbc:sqlite:"+DATABASE_NAME;
+    private static final String CONNECTION="jdbc:sqlite:"+DATABASE_NAME;
 
     private Connection conn;
     public boolean open(){
@@ -38,54 +38,54 @@ public class DataSource {
     }
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //CREATE TABLE IF NOT EXISTS company( _id INTEGER, name TEXT)
-    public static final String TABLE_COMPANY="company_info";
-    public static final String COLUMN_COMPANY_ID="_id";
-    public static final String COLUMN_COMPANY_NAME="name";
-    public static final String CREATE="CREATE TABLE IF NOT EXISTS ";
-    public static final String QUERY_CREATE_TABLE_COMPANY=CREATE+TABLE_COMPANY+
+    private static final String TABLE_COMPANY="company_info";
+    private static final String COLUMN_COMPANY_ID="_id";
+    private static final String COLUMN_COMPANY_NAME="name";
+    private static final String CREATE="CREATE TABLE IF NOT EXISTS ";
+    private static final String QUERY_CREATE_TABLE_COMPANY=CREATE+TABLE_COMPANY+
             "("+COLUMN_COMPANY_ID+" INTEGER PRIMARY KEY, "+COLUMN_COMPANY_NAME+" TEXT NOT NULL)";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create department table
-    public static final String TABLE_DEPARTMENT="department_info";
-    public static final String COLUMN_DEPARTMENT_ID="_id";
-    public static final String COLUMN_DEPARTMENT_NAME="name";
-    public static final String QUERY_CREATE_TABLE_DEPARTMENT=CREATE+TABLE_DEPARTMENT+
+    private static final String TABLE_DEPARTMENT="department_info";
+    private static final String COLUMN_DEPARTMENT_ID="_id";
+    private static final String COLUMN_DEPARTMENT_NAME="name";
+    private static final String QUERY_CREATE_TABLE_DEPARTMENT=CREATE+TABLE_DEPARTMENT+
             "("+COLUMN_DEPARTMENT_ID+" INTEGER PRIMARY KEY, "+COLUMN_DEPARTMENT_NAME+" TEXT NOT NULL)";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create userLoginInfoTable
-    public static final String TABLE_USER_LOGIN_INFO="user_login_info";
-    public static final String COLUMN_USER_LOGIN_INFO_ID="_id";
-    public static final String COLUMN_USER_LOGIN_INFO_FIRST_NAME="firstName";
-    public static final String COLUMN_USER_LOGIN_INFO_LAST_NAME="lastName";
-    public static final String COLUMN_USER_LOGIN_INFO_EMAIL="emailId";
-    public static final String COLUMN_USER_LOGIN_INFO_GENDER="gender";
-    public static final String COLUMN_USER_LOGIN_INFO_COMPANY="company";
-    public static final String QUERY_CREATE_TABLE_USER_LOGIN_INFO=CREATE+TABLE_USER_LOGIN_INFO+
+    private static final String TABLE_USER_LOGIN_INFO="user_login_info";
+    private static final String COLUMN_USER_LOGIN_INFO_ID="_id";
+    private static final String COLUMN_USER_LOGIN_INFO_FIRST_NAME="firstName";
+    private static final String COLUMN_USER_LOGIN_INFO_LAST_NAME="lastName";
+    private static final String COLUMN_USER_LOGIN_INFO_EMAIL="emailId";
+    private static final String COLUMN_USER_LOGIN_INFO_GENDER="gender";
+    private static final String COLUMN_USER_LOGIN_INFO_COMPANY="company";
+    private static final String QUERY_CREATE_TABLE_USER_LOGIN_INFO=CREATE+TABLE_USER_LOGIN_INFO+
             "("+COLUMN_USER_LOGIN_INFO_ID+" INTEGER PRIMARY KEY, "+COLUMN_USER_LOGIN_INFO_FIRST_NAME+" TEXT NOT NULL,"+
             COLUMN_USER_LOGIN_INFO_LAST_NAME+" TEXT NOT NULL,"+COLUMN_USER_LOGIN_INFO_EMAIL+" TEXT NOT NULL UNIQUE ,"+COLUMN_USER_LOGIN_INFO_GENDER
             +" TEXT NOT NULL,"+COLUMN_USER_LOGIN_INFO_COMPANY+" INTEGER NOT NULL)";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create login credentials table
-    public static final String TABLE_LOGIN_CREDENTIALS_INFO="login_credentials";
-    public static final String COLUMN_LOGIN_CREDENTIALS_INFO_ID ="_id";
-    public static final String COLUMN_LOGIN_CREDENTIALS_INFO_UserId ="useId";
-    public static final String COLUMN_LOGIN_CREDENTIALS_INFO_Password ="password";
-    public static final String QUERY_CREATE_TABLE_LOGIN_CREDENTIALS_INFO=CREATE+TABLE_LOGIN_CREDENTIALS_INFO+
+    private static final String TABLE_LOGIN_CREDENTIALS_INFO="login_credentials";
+    private static final String COLUMN_LOGIN_CREDENTIALS_INFO_ID ="_id";
+    private static final String COLUMN_LOGIN_CREDENTIALS_INFO_UserId ="useId";
+    private static final String COLUMN_LOGIN_CREDENTIALS_INFO_Password ="password";
+    private static final String QUERY_CREATE_TABLE_LOGIN_CREDENTIALS_INFO=CREATE+TABLE_LOGIN_CREDENTIALS_INFO+
             "("+ COLUMN_LOGIN_CREDENTIALS_INFO_ID +" INTEGER PRIMARY KEY, "+ COLUMN_LOGIN_CREDENTIALS_INFO_UserId +" TEXT NOT NULL UNIQUE ,"+
             COLUMN_LOGIN_CREDENTIALS_INFO_Password +" TEXT)";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create project_info
-    public static final String TABLE_PROJECT_INFO="project_info";
-    public static final String COLUMN_PROJECT_INFO_ID="_id";
-    public static final String COLUMN_PROJECT_INFO_PROJECT_NAME="project_Name";
-    public static final String COLUMN_PROJECT_INFO_LEADER_NAME="leader_Name";
-    public static final String COLUMN_PROJECT_INFO_COMPANY="company";
-    public static final String COLUMN_PROJECT_INFO_DEPARTMENT="department";
-    public static final String COLUMN_PROJECT_INFO_MILESTONE_DATE="milestone_date";
-    public static final String COLUMN_PROJECT_INFO_END_DATE="end_date";
-    public static final String COLUMN_PROJECT_INFO_ACTUAL_START_DATE="actualStart_date";
-    public static final String COLUMN_PROJECT_INFO_CHECK_DATE="check_date";
-    public static final String QUERY_CREATE_TABLE_PROJECT_INFO=CREATE+TABLE_PROJECT_INFO+
+    private static final String TABLE_PROJECT_INFO="project_info";
+    private static final String COLUMN_PROJECT_INFO_ID="_id";
+    private static final String COLUMN_PROJECT_INFO_PROJECT_NAME="project_Name";
+    private static final String COLUMN_PROJECT_INFO_LEADER_NAME="leader_Name";
+    private static final String COLUMN_PROJECT_INFO_COMPANY="company";
+    private static final String COLUMN_PROJECT_INFO_DEPARTMENT="department";
+    private static final String COLUMN_PROJECT_INFO_MILESTONE_DATE="milestone_date";
+    private static final String COLUMN_PROJECT_INFO_END_DATE="end_date";
+    private static final String COLUMN_PROJECT_INFO_ACTUAL_START_DATE="actualStart_date";
+    private static final String COLUMN_PROJECT_INFO_CHECK_DATE="check_date";
+    private static final String QUERY_CREATE_TABLE_PROJECT_INFO=CREATE+TABLE_PROJECT_INFO+
             "("+COLUMN_PROJECT_INFO_ID+" INTEGER PRIMARY KEY, "+COLUMN_PROJECT_INFO_PROJECT_NAME+" TEXT NOT NULL UNIQUE, "+
             COLUMN_PROJECT_INFO_LEADER_NAME+" TEXT NOT NULL, " +COLUMN_PROJECT_INFO_COMPANY+" INTEGER NOT NULL, "+
             COLUMN_PROJECT_INFO_DEPARTMENT+" INTEGER NOT NULL, "+COLUMN_PROJECT_INFO_MILESTONE_DATE+" TEXT NOT NULL, "
@@ -93,21 +93,21 @@ public class DataSource {
             COLUMN_PROJECT_INFO_CHECK_DATE+" TEXT NOT NULL)";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create project_planning_info
-    public static final String TABLE_PROJECT_PLANING_INFO="planing_phase_info";
+    private static final String TABLE_PROJECT_PLANING_INFO="planing_phase_info";
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create project_execution_info
-    public static final String TABLE_PROJECT_EXECUTION_INFO="execution_phase_info";
+    private static final String TABLE_PROJECT_EXECUTION_INFO="execution_phase_info";
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     //to create project_analysis_info
-    public static final String TABLE_PROJECT_ANALYSIS_INFO="analysis_info";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_ID="_id";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_CC="cc";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_PB="pb";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_PRIORITY="priority";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_REMAINING_WAIT="remaining_wait";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_STATUS="status";
-    public static final String COLUMN_PROJECT_ANALYSIS_INFO_SCHEDULED_START_DATE="scheduled_start";
+    private static final String TABLE_PROJECT_ANALYSIS_INFO="analysis_info";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_ID="_id";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_CC="cc";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_PB="pb";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_PRIORITY="priority";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_REMAINING_WAIT="remaining_wait";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_STATUS="status";
+    private static final String COLUMN_PROJECT_ANALYSIS_INFO_SCHEDULED_START_DATE="scheduled_start";
 
     public static final String QUERY_CREATE_TABLE_PROJECT_ANALYSIS_INFO=CREATE+TABLE_PROJECT_ANALYSIS_INFO+
             "("+COLUMN_PROJECT_ANALYSIS_INFO_ID+" INTEGER PRIMARY KEY, "+COLUMN_PROJECT_ANALYSIS_INFO_CC+" INTEGER NOT NULL, "
@@ -143,7 +143,19 @@ public class DataSource {
 
 
     }
+//must happen when ever logged in
 
+    public void addDataIntoTable(){
+
+    }
+
+    public void deleteFromTable(){
+
+    }
+
+    public void updateTable(){
+
+    }
     //add data
     //delete data
     //update data
