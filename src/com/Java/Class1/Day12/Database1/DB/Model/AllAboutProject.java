06da29bcb4a9433e43bd32b2a48c992957ecfabc
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-03-29 08:48:26 +0900

    Projects info

--- src/com/Java/Class1/Day12/Database1/DB/Model/AllAboutProject.java
+++ src/com/Java/Class1/Day12/Database1/DB/Model/AllAboutProject.java
@@ -1,5 +1,8 @@
 package com.Java.Class1.Day12.Database1.DB.Model;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -8,24 +11,104 @@ package com.Java.Class1.Day12.Database1.DB.Model;
 public class AllAboutProject {
     //////////////////////////
     private int companyId;
-    private String CompanyName;
+    private SimpleStringProperty CompanyName;
     /////////////////////////
     private int departmentId;
-    private String departmentName;
+    private SimpleStringProperty departmentName;
     ////////////////////////
     private int userId;
-    private String fName;
-    private String lName;
-    private String email;
-    private String gender;
+    private SimpleStringProperty fName;
+    private SimpleStringProperty lName;
+    private SimpleStringProperty email;
+    private SimpleStringProperty gender;
     ////////////////////////
-    private String loginId;
-    private String password;
+    private SimpleStringProperty loginId;
+    private SimpleStringProperty password;
     ////////////////////////
-    private String projectName;
-    private String leaderName;
-
-
+    private SimpleStringProperty projectName;
+    private SimpleStringProperty leaderName;
+    private SimpleStringProperty milestone_Date;
+    private SimpleStringProperty end_Date;
+    private SimpleStringProperty actual_Date;
+    private SimpleStringProperty check_Date;
+    ////////////////////////
+    //analysis//
+    private SimpleStringProperty status;
+    private SimpleDoubleProperty cc;
+    private SimpleDoubleProperty pb;
+    private SimpleDoubleProperty priority;
+    private SimpleDoubleProperty remaining_wait;
+    private SimpleStringProperty Scheduled_Start_Date;
+    private SimpleDoubleProperty Xaxis;
+    private SimpleDoubleProperty Yaxis;
+    ///////////////////////
+    //planing//
+    private SimpleStringProperty currentTask;
+    private SimpleDoubleProperty T1;
+    private SimpleDoubleProperty T2;
+    private SimpleDoubleProperty T3;
+    private SimpleDoubleProperty T4;
+    private SimpleDoubleProperty T5;
+    private SimpleDoubleProperty T6;
+    private SimpleDoubleProperty T7;
+    private SimpleDoubleProperty T8;
+    private SimpleDoubleProperty T9;
+    private SimpleDoubleProperty T10;
+    private SimpleDoubleProperty T11;
+    private SimpleDoubleProperty T12;
+    private SimpleDoubleProperty planning_weight;
+    private SimpleStringProperty wait_Criteria;
+    private SimpleStringProperty task_Criteria;
+    private SimpleDoubleProperty R1;
+    private SimpleDoubleProperty R2;
+    private SimpleDoubleProperty R3;
+    private SimpleDoubleProperty R4;
+    private SimpleDoubleProperty R5;
+    private SimpleDoubleProperty R6;
+    private SimpleDoubleProperty R7;
+    private SimpleDoubleProperty R8;
+    private SimpleDoubleProperty R9;
+    private SimpleDoubleProperty R10;
+    private SimpleDoubleProperty R11;
+    private SimpleDoubleProperty R12;
+    /////////////////////
+    //execution//
+    private SimpleDoubleProperty Te1;
+    private SimpleDoubleProperty Te2;
+    private SimpleDoubleProperty Te3;
+    private SimpleDoubleProperty Te4;
+    private SimpleDoubleProperty Te5;
+    private SimpleDoubleProperty Te6;
+    private SimpleDoubleProperty Te7;
+    private SimpleDoubleProperty Te8;
+    private SimpleDoubleProperty Te9;
+    private SimpleDoubleProperty Te10;
+    private SimpleDoubleProperty Te11;
+    private SimpleDoubleProperty Te12;
+    private SimpleDoubleProperty we1;
+    private SimpleDoubleProperty we2;
+    private SimpleDoubleProperty we3;
+    private SimpleDoubleProperty we4;
+    private SimpleDoubleProperty we5;
+    private SimpleDoubleProperty we6;
+    private SimpleDoubleProperty we7;
+    private SimpleDoubleProperty we8;
+    private SimpleDoubleProperty we9;
+    private SimpleDoubleProperty we10;
+    private SimpleDoubleProperty we11;
+    private SimpleDoubleProperty we12;
+    private SimpleDoubleProperty Re1;
+    private SimpleDoubleProperty Re2;
+    private SimpleDoubleProperty Re3;
+    private SimpleDoubleProperty Re4;
+    private SimpleDoubleProperty Re5;
+    private SimpleDoubleProperty Re6;
+    private SimpleDoubleProperty Re7;
+    private SimpleDoubleProperty Re8;
+    private SimpleDoubleProperty Re9;
+    private SimpleDoubleProperty Re10;
+    private SimpleDoubleProperty Re11;
+    private SimpleDoubleProperty Re12;
 
     //getters and setters
 
