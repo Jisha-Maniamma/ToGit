05e0a1af353337ee543f63928d0bb67d76599787
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-04-08 13:15:35 +0900

    Just Java Basics

---
+++ src/com/Java/Class1/Day13/Day4/Main1.java
@@ -0,0 +1,17 @@
+package com.Java.Class1.Day13.Day4;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/04/08 10:40
+ */
+public class Main1 {
+    public static void main(String[] k) {
+
+        k= new String[]{"one","two"};
+
+        System.out.println("first value is: "+k[0]+", Second value is: "+k[1]);
+
+        System.out.println(k instanceof Object);
+    }
+}
