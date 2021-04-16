1a1079fa29147404e95c626fe85622d8e09c6569
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-04-16 11:48:07 +0900

    JUnit testing example

--- src/com/Java/Class1/Day15/mainClass/BankingTest.java
+++ src/com/Java/Class1/Day15/mainClass/BankingTest.java
@@ -11,20 +11,27 @@ class BankingTest {
 
     @org.junit.jupiter.api.Test
     void deposit() {
-      Banking account1=new Banking("Jisha","Maniamm",1000);
+      Banking account1=new Banking("Jisha","Maniamma",1000);
       double balance=account1.deposit(200);
       assertEquals(1200,balance,0);
     }
 
     @org.junit.jupiter.api.Test
     void withdraw() {
-        fail("yet to be implimented");
+        Banking account1=new Banking("Jisha","Maniamma",1000);
+        double balance=account1.withdraw(200);
+        assertEquals(800,balance,0);
     }
 
     @org.junit.jupiter.api.Test
-    void getBalance() {
+    void getBalance_after_withdraw() {
         fail("yet to be implimented");
     }
+    @org.junit.jupiter.api.Test
+    void getBalance_after_dposit() {
+        fail("yet to be implimented");
+    }
+
     @org.junit.jupiter.api.Test
     void TestDummy(){
         assertEquals(20,21);
