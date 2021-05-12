d6cf3bf6792fb542bbc476a4b01fa8ba2eda3460
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-12 09:42:15 +0900

    2021/05/12 Array and String object reference understanding

--- src/com/Java/Class1/Day18/Arrayexampl.java
+++ src/com/Java/Class1/Day18/Arrayexampl.java
@@ -39,7 +39,7 @@ public class Arrayexampl {
         System.out.println(s2.hashCode());
         System.out.println(s1==s2);
         System.out.println(s1.equals(s2));
-        s1.replace("i","a");
+        s1=s1.replace("i","a");
         System.out.println(s1==s2);
         System.out.println(s1.equals(s2));
         System.out.println("///////////////////////////");
