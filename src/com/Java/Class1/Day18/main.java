f11b6b94648677bb276c8d1733b475de1484a59f
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-18 07:46:15 +0900

    Autoboxing and Unboxing

--- src/com/Java/Class1/Day18/main.java
+++ src/com/Java/Class1/Day18/main.java
@@ -26,20 +26,30 @@ public class main {
         Bank bank1=new Bank("State Bank of India");
         bank1.AddBranch("AnuVijay T/S");
         bank1.addCustomer("AnuVijay T/S","Jisha",1200);
+
         bank1.addCustomer("AnuVijay T/S","Manish",10000);
+
         bank1.addCustomer("AnuVijay T/S","Gopi",8100);
         bank1.addCustomer("AnuVijay T/S","Mani",3200);
 
         bank1.AddBranch("Adinadu");
         bank1.addCustomer("Adinadu","Arun",1200);
 
-bank1.AddCustomerTransaction("Adinadu","Arun",200);
+        bank1.AddCustomerTransaction("Adinadu","Arun",200);
         bank1.AddCustomerTransaction("Adinadu","Arun",100);
         bank1.AddCustomerTransaction("Adinadu","Arun",50);
 
         bank1.AddCustomerTransaction("AnuVijay T/S","Jisha",200);
+        System.out.println( bank1.getBankBranch().get(0).getBranchName());
 
-        bank1.listCutomers("AnuVijay T/S",true);
+        System.out.println( bank1.getBankBranch().get(1).getBranchName());
+
+        System.out.println();
+        System.out.println("..................................");
+        System.out.println("..................................");
+        bank1.listCutomers(bank1.getBankBranch().get(0).getBranchName(),true);
+        System.out.println("..................................");
+        System.out.println("..................................");
 
 
     }
