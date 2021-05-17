3a26acafc3b7e0b999acbd901fa21f4eb8b97600
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-17 16:23:34 +0900

    Autoboxing and unboxing

--- src/com/Java/Class1/Day18/Bank.java
+++ src/com/Java/Class1/Day18/Bank.java
@@ -1,10 +1,24 @@
 package com.Java.Class1.Day18;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
  * @date 2021/05/17 14:48
  */
 public class Bank {
+
+    private List<Branch> BankBranch;
+    private String name;
+
+    public Bank(String BankName){
+        this.name=BankName;
+        this.BankBranch=new ArrayList<Branch>();
+    }
+
+
+
 
 }
