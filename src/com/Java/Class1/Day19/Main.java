52db4332624a77a3ed04119ff23826098de7b435
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-19 07:20:38 +0900

    interface

---
+++ src/com/Java/Class1/Day19/Main.java
@@ -0,0 +1,14 @@
+package com.Java.Class1.Day19;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/05/19 7:17
+ */
+public class Main {
+    public static void main(String[] args) {
+String name="JiSha";
+String name1="jisha";
+        System.out.println(name.equalsIgnoreCase(name1));
+    }
+}
