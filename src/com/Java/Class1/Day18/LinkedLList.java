ebe54c51079183297d744ab41008040d6b7d324d
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-18 09:40:54 +0900

    Autoboxing and Unboxing

---
+++ src/com/Java/Class1/Day18/LinkedLList.java
@@ -0,0 +1,78 @@
+package com.Java.Class1.Day18;
+
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/05/18 8:20
+ */
+public class LinkedLList {
+    public static void main(String[] args) {
+        // List<String> names=new ArrayList<>();
+        LinkedList<String> ll
+                = new LinkedList<String>();
+
+        ll.add("iisha9");
+        ll.add("jisha2");
+        ll.add("kisha3");
+        ll.add("isha4");
+        ll.add("jisha5");
+        ll.add("misha6");
+        ll.add("jisha7");
+        ll.add("jisha8");
+
+        ListIterator<String> iterator=ll.listIterator();
+        int i=-1;
+        while(iterator.hasNext()){
+
+            System.out.println(iterator.next());
+
+        }
+        System.out.println("..............");
+        ll.remove(1);
+        System.out.println("...................");
+        ListIterator<String> iterator1=ll.listIterator();
+        while(iterator1.hasNext()){
+
+            System.out.println(iterator1.next());
+
+        }
+
+        List<String> slist = Arrays.asList("Tanu", "Kamal", "Suman", "Lucky", "Bunty", "Amit");
+        List<String> sortedList = slist.stream().sorted().collect(Collectors.toList());
+        sortedList.forEach(System.out::println);
+
+        System.out.println("*********************************************************");
+
+        AddNewName(ll,"lishaaaaa");
+        iterator1=ll.listIterator();
+        while(iterator1.hasNext()){
+
+            System.out.println(iterator1.next());
+
+        }
+    }
+
+    public static boolean AddNewName(List<String> a,String name){
+        ListIterator<String> iterator=a.listIterator();
+        while(iterator.hasNext()){
+            int compare=iterator.next().compareTo(name);
+
+            if (compare>0){
+                iterator.previous();
+                iterator.add(name);
+                return true;
+            }
+
+
+        }
+        iterator.add(name);
+        return  false;
+    }
+}
