ead62dd0cd14dca4a6f63ecc26926598393d455b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-19 08:25:38 +0900

    interface

---
+++ src/com/Java/Class1/Day19/Telephone.java
@@ -0,0 +1,12 @@
+package com.Java.Class1.Day19;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/05/19 7:24
+ */
+public interface Telephone {
+    void powerOn();
+    void dail(int phoneNumber);
+    boolean isRinging();
+}
