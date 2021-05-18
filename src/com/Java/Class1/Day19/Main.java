79eac8abe6ad5591f61445e0dc4a274e09f415ed
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-19 07:41:06 +0900

    interface

--- src/com/Java/Class1/Day19/Main.java
+++ src/com/Java/Class1/Day19/Main.java
@@ -7,8 +7,17 @@ package com.Java.Class1.Day19;
  */
 public class Main {
     public static void main(String[] args) {
-String name="JiSha";
-String name1="jisha";
+        String name="JiSha";
+        String name1="jisha";
         System.out.println(name.equalsIgnoreCase(name1));
+        Telephone JishaLandlinePhone=new DeskPhone();
+        JishaLandlinePhone.dail(1234);
+        System.out.println("..........................");
+
+        Telephone JishaMobilePhone=new mobilePhone(1234);
+        JishaMobilePhone.powerOn();
+        JishaMobilePhone.isRinging();
     }
+
+
 }
