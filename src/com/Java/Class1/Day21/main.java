aba1f514cb96a45a91fbdd577b8a2ff209703a28
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-24 08:37:05 +0900

    inner class

--- src/com/Java/Class1/Day21/main.java
+++ src/com/Java/Class1/Day21/main.java
@@ -6,4 +6,18 @@ package com.Java.Class1.Day21;
  * @date 2021/05/21 8:01
  */
 public class main {
+
+    public static void main(String[] args) {
+Outer o=new Outer();
+Outer.inner i=o.new inner();
+
+
+        System.out.println(i.y+o.x);
+    }
+}
+class Outer{
+    int x=12;
+    class inner{
+        int y=20;
+    }
 }
