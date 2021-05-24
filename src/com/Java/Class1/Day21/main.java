ef33403560ea168576eda828bac1063dd774a1d3
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-25 08:12:24 +0900

    25/5/2021

--- src/com/Java/Class1/Day21/main.java
+++ src/com/Java/Class1/Day21/main.java
@@ -14,14 +14,47 @@ Outer.inner i=o.new inner();
 Outer.inner1 i1=new Outer.inner1();
         System.out.println(i.y+o.x);
         System.out.println(i1.z);
+
+
+        ///access outer class fom inner class
+
+        System.out.println(i.myInnermethod());
+////////////////////////////////////////////////////////////////
+        System.out.println("..............................");
+
+        A_Outer outer1=new A_Outer();
+        A_Outer.A_inner1 inner1=outer1.new A_inner1();
+        System.out.println(inner1.getXvalue());
+
     }
 }
 class Outer{
     int x=12;
     class inner{
+        public int myInnermethod(){
+            return x;
+        }
+
         int y=20;
     }
     static class inner1{
         int z=30;
+
+    }
+}
+
+class A_Outer{
+    int x=10;
+    class A_inner1{
+    public int getXvalue(){
+        return x;
+    }
+
+        int y=20;
+    }
+
+    static class A_inner2{
+        int z=5;
+
     }
 }
