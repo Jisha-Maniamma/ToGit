aee416cb7a4aa408835975afe28dfa5dd64150cc
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-28 15:25:23 +0900

    28/05/2021 lambda expression

--- src/com/Java/Class1/Day22/main.java
+++ src/com/Java/Class1/Day22/main.java
@@ -1,9 +1,45 @@
 package com.Java.Class1.Day22;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
  * @date 2021/05/24 7:56
  */
 public class main {
+    public static String To_Load_FILE="src/com/Java/Class1/Day22/Holidays_Entry_SheetV2.txt";
+
+    public static void main(String[] args) throws IOException {
+        LoadFrom(To_Load_FILE);
+
+    }
+
+
+    public static void LoadFrom(String to_Load_FILE) throws IOException {
+
+
+        // table.getItems().clear();
+        DateTimeFormatter df=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Path path= Paths.get(to_Load_FILE);
+        BufferedReader br= Files.newBufferedReader(path);
+        String input;
+
+        try{
+            System.out.println("loading from file- "+to_Load_FILE);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+    }
+
 }
