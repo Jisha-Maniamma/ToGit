53aa6f9385539582bc78bd5608decf0d13b93be3
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-05-28 09:33:14 +0900

    28/05/2021 lambda expression

--- src/com/Java/Class1/Day25/Test.java
+++ src/com/Java/Class1/Day25/Test.java
@@ -1,5 +1,10 @@
 package com.Java.Class1.Day25;
 
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -8,6 +13,31 @@ package com.Java.Class1.Day25;
 public class Test {
 
     public static void main(String[] args) {
+        Emp emp1=new Emp("Krishna",6,12300);
+        Emp emp2=new Emp("Jisha",1,12300);
+
+        List<Emp> empArrayList=new ArrayList<>();
+        empArrayList.add(emp1);
+        empArrayList.add(emp2);
+
+        filter(empArrayList,new ValidatePerformanceRating());
+
+
+        System.out.println();
+        System.out.println("//////////////////////////////////////");
+
+        validate validatearray=e -> e.getPerformance()>=5;
+        filter(empArrayList,validatearray);
+
+
+    }
+
+    private static void filter(List<Emp> empArrayList, validate rule) {
+        for(Emp e:empArrayList){
+            if(rule.check(e)){
+                System.out.println(e);
+            }
+        }
 
     }
 }
@@ -41,5 +71,10 @@ class Emp{
 
     public double getSalary() {
         return salary;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
