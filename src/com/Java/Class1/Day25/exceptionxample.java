60153de638284f94e973e12951b1d1685c6e4974
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-07 07:18:28 +0900

    06/07/2021

--- src/com/Java/Class1/Day25/exceptionxample.java
+++ src/com/Java/Class1/Day25/exceptionxample.java
@@ -6,8 +6,15 @@ package com.Java.Class1.Day25;
  * @date 2021/05/31 7:30
  */
 public class exceptionxample {
+    static void getValue() throws Exp1 {
+        throw new Exp1("");
+    }
 
     public static void main(String[] args) throws  Exp1 {
+
+
+
+        prrrrint();
         printAAAA a=new printAAAA();
 
       //  a.getPrinterName(2);
@@ -25,16 +32,24 @@ public class exceptionxample {
        //
 
     }
+
+    private static void prrrrint()  {
+        int a=1;
+      //  if(a==1) throw new Exp1("");
+        if(a>0) throw new IllegalAccessError("error");
+
+
+    }
 }
 class printAAAA{
     void getPrinterName(int n) throws Exp1{
         if(n<3)
-            throw new Exp1();
+            throw new Exp1("");
 
     }
 }
 class Exp1 extends Exception{
-    public Exp1() {
-        System.out.println("its exception");
+    public Exp1(String s) {
+        super();
     }
 }
