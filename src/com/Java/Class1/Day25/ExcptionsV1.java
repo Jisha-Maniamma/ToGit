242af99fb00b8c94cd4a2c431f2e5ce15a095afc
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-07 07:18:41 +0900

    06/07/2021

---
+++ src/com/Java/Class1/Day25/ExcptionsV1.java
@@ -0,0 +1,23 @@
+package com.Java.Class1.Day25;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/05/31 14:55
+ */
+public class ExcptionsV1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("///////Exception///////");
+        int x=21;
+        int y=0;
+        System.out.println(22/0);
+        System.out.println("///////////////////////");
+            throw new Exception();
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+    }
+}
