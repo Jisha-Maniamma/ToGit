444b81b9846e67a047e5f2e3d6358f3d4523b5cd
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-07 07:25:57 +0900

    06/07/2021

--- src/com/Java/Class1/Day28/mainPrimitiveClass.java
+++ src/com/Java/Class1/Day28/mainPrimitiveClass.java
@@ -6,4 +6,23 @@ package com.Java.Class1.Day28;
  * @date 2021/06/07 7:18
  */
 public class mainPrimitiveClass {
+
+    public static void main(String...args) {
+        System.out.println("Java");
+
+        String i=new String("2Hai");
+        System.out.println(i);
+        modify(i);
+        System.out.println(i);
+        i=modify(i);
+        System.out.println(i);
+    }
+
+    private static String modify(String i) {
+       return i.concat("123");
+    }
+
+//    public static void modify(String i){
+//
+//    }
 }
