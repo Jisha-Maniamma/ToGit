860ed138ba8d784fdfb1efa5272cf89574615d36
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-29 07:09:32 +0900

    2021/06/29

---
+++ src/com/Java/Class1/Day34_DB/Main.java
@@ -0,0 +1,13 @@
+package com.Java.Class1.Day34_DB;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/06/24 7:16
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("hai");
+//        new A();
+    }
+}
