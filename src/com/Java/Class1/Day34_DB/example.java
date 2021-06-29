39796ac73e792b246b6bf0cac3cdf6ecdae9f42a
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-30 07:11:20 +0900

    2021/06/30

---
+++ src/com/Java/Class1/Day34_DB/example.java
@@ -0,0 +1,16 @@
+package com.Java.Class1.Day34_DB;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/06/29 7:07
+ */
+public class example {
+    private static String name="jisha";
+
+    public static void main(String[] args) {
+        System.out.println(name=name+" hai");
+        System.out.println(name);
+    }
+
+}
