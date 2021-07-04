b3db28e171dace63309e59e7d266e85ccf05abae
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-05 07:24:46 +0900

    while-do-while

---
+++ src/com/Java/Class1/Day35/chapter5.java
@@ -0,0 +1,35 @@
+package com.Java.Class1.Day35;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/07/05 7:07
+ */
+public class chapter5 {
+    public static void main(String[] args) {
+        int[] a=new int[5];
+        for(int i=0;i<a.length;i++){
+            a[i]=i;
+            if((a[i]%2)==0)
+                a[i]=20;
+        }
+        for (int b:a) {
+            System.out.println(b+=2);
+        }
+        System.out.println("////////////////////");
+        for (int b:a) {
+            System.out.println(b);
+        }
+
+        System.out.println("////////////////////");
+        int i=10;
+        do
+            while(++i <15)
+            //    System.out.println(i);
+                i=i+20;
+               // System.out.println(i+" is");}
+        while (i<2);
+        System.out.println(i);
+
+    }
+}
