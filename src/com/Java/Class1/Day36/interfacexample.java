da08a9324e72fb8c444c6be7fba28ae9bdfd3149
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-06 07:35:36 +0900

    2021/07/06

---
+++ src/com/Java/Class1/Day36/interfacexample.java
@@ -0,0 +1,26 @@
+package com.Java.Class1.Day36;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/07/06 7:18
+ */
+public class interfacexample implements  interviewer{
+    public static void main(String[] args) {
+        interviewer i=new Manager();
+        i.submitIntrviewStatu();
+    }
+}
+class Manager implements interviewer{
+    public Object submitIntrviewStatu(){
+        System.out.println("Manager:Accept");
+        return null;
+    }
+}
+interface interviewer{
+
+    default Object submitIntrviewStatu(){
+        System.out.println("interviewer:Accept");
+        return null;
+    }
+}
