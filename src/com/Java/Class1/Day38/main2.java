2f2151d4c8691fbb4331bb00cc111ef2924d923b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-07 08:08:29 +0900

    exception handeling examples

---
+++ src/com/Java/Class1/Day38/main2.java
@@ -0,0 +1,27 @@
+package com.Java.Class1.Day38;
+
+import java.time.LocalTime;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/07/07 7:55
+ */
+public class main2 {
+    public static void main(String[] args) {
+main2 m=new main2();
+m.goTreaking();
+    }
+
+    private void goTreaking() {
+
+        LocalTime hrs= LocalTime.now();
+        if(hrs==LocalTime.now())throw new fellHunrgyException();
+    }
+
+
+}
+
+class fellHunrgyException extends RuntimeException{
+
+}
