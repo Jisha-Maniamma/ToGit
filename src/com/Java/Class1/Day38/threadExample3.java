c7e3706113553e7a438cbde38961419cf1e5f871
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-07 13:37:26 +0900

    Threads in Java

--- src/com/Java/Class1/Day38/threadExample3.java
+++ src/com/Java/Class1/Day38/threadExample3.java
@@ -8,10 +8,10 @@ package com.Java.Class1.Day38;
 public class threadExample3 {
 
     public static void main(String[] args) {
-        System.out.println("hello from main thread");
+        System.out.println(ThreadColor.blue+"hello from main thread");
         Thread a=new anotherThread();
         a.start();
-        System.out.println("hello again from main tread");
+        System.out.println(ThreadColor.Purple+"hello again from main tread");
 
     }
 }
@@ -24,6 +24,10 @@ class anotherThread extends Thread{
 }
 class ThreadColor{
     public static final String Green="\u001B[32m";
-
+    public static final String Red="\u001B[31m";
+    public static final String Purple="\u001B[35m";
+    public static final String cyan="\u001B[36m";
+    public static final String blue="\u001B[34m";
+    public static final String black="\u001B[30m";
 
         }
