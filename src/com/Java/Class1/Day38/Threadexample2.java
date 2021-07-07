6da175906c6f8f8809da3f4fbb83e9f8a1cfc03c
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-07 13:29:45 +0900

    Threads in Java

--- src/com/Java/Class1/Day38/Threadexample2.java
+++ src/com/Java/Class1/Day38/Threadexample2.java
@@ -31,7 +31,7 @@ class RunnableDemo implements Runnable {
             for(int i = 4; i > 0; i--) {
                 System.out.println("Thread: " + threadName + ", " + i);
                 // Let the thread sleep for a while.
-                Thread.sleep(50);
+                Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
             System.out.println("Thread " +  threadName + " interrupted.");
