33f84294a27ec608ff92b66ae6a6e4552e53e6a0
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-07 11:41:33 +0900

    Threads in Java

---
+++ src/com/Java/Class1/Day37/main.java
@@ -0,0 +1,13 @@
+package com.Java.Class1.Day37;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/07/06 12:48
+ */
+public class main {
+    public static void main(String[] args) {
+
+
+    }
+}
