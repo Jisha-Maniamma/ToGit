729052ed9cf32506d2416a1a46929c00993b0468
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-08 15:42:21 +0900

    2021/07/08

---
+++ src/com/Java/Class1/Day39/Thread1.java
@@ -0,0 +1,30 @@
+package com.Java.Class1.Day39;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/07/08 15:22
+ */
+public class Thread1 extends Thread implements Runnable{
+
+    public void run(){
+        for(int i=0;i<5;i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+       new Thread1().start();
+       Thread t=new Thread(new Thread1());
+       t.start();
+        Thread t1=new Thread(new Thread1());
+        t1.start();
+
+    }
+}
