6e532f777d9d15b91a06a3e1dc9318e403af87d0
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-07-08 15:43:10 +0900

    2021/07/08

--- src/com/Java/Class1/Day39/Thread1.java
+++ src/com/Java/Class1/Day39/Thread1.java
@@ -8,7 +8,7 @@ package com.Java.Class1.Day39;
 public class Thread1 extends Thread implements Runnable{
 
     public void run(){
-        for(int i=0;i<5;i++){
+        for(int i=0;i<100;i++){
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
