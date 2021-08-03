8b85ce3d958106947e2af576c3ae8fd1bc4e68b3
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-03 10:43:07 +0900

    03/08/2021

---
+++ src/com/Java/Class1/Day41/main.java
@@ -0,0 +1,13 @@
+package com.Java.Class1.Day41;
+
+/**
+ * @author Jisha Maniamma
+ * @version 1.0
+ * @date 2021/08/03 8:12
+ */
+public class main {
+    public static void main(String[] args) {
+        String a="/*faf*/hllo/*faf*/das/*klnvlkdv"+
+              " */ bcbvcbv";
+    }
+}
