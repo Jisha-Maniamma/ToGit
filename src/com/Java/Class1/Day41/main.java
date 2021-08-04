4fcf8a59a654a14da0e9d2bde7248fba4b56520c
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-05 08:10:45 +0900

    2021/08/05

--- src/com/Java/Class1/Day41/main.java
+++ src/com/Java/Class1/Day41/main.java
@@ -1,5 +1,8 @@
 package com.Java.Class1.Day41;
 
+
+import java.util.ArrayList;
+
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -9,5 +12,25 @@ public class main {
     public static void main(String[] args) {
         String a="/*faf*/hllo/*faf*/das/*klnvlkdv"+
               " */ bcbvcbv";
+
+
+        ArrayList<StringBuilder> myrraylist=new ArrayList<StringBuilder>();
+        StringBuilder sb1=new StringBuilder("jan");
+        StringBuilder sb2=new StringBuilder("feb");
+        myrraylist.add(sb1);
+        myrraylist.add(sb2);
+//        System.out.println(myrraylist.toString().contains("jan"));
+
+
+
     }
+
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(obj instanceof StringBuilder){
+//            StringBuilder sb=(StringBuilder) obj;
+//            return sb.equals(this);
+//        }
+//        return false;
+//    }
 }
