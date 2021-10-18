f0a7f0ad5f5dae1029d56e0b568d9b5294217b5e
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-10-19 07:24:33 +0900

    19/10/2021

--- src/com/Java/Class1/Day42/challngearaylist/contacts.java
+++ src/com/Java/Class1/Day42/challngearaylist/contacts.java
@@ -1,11 +1,24 @@
 package com.Java.Class1.Day42.challngearaylist;
 
-import lombok.Data;
 
-@Data
 public class contacts {
 
     private String name;
     private String number;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
