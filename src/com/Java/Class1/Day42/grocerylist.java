11e6d3cbc4ccc90f43937fbf9e01f2dec510838d
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-10-18 13:46:29 +0900

    18/10/2021

---
+++ src/com/Java/Class1/Day42/grocerylist.java
@@ -0,0 +1,25 @@
+package com.Java.Class1.Day42;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class grocerylist {
+    private List<String> grocerylist=new ArrayList<>();
+
+
+    public void adGroceryLit(String item){
+        grocerylist.add(item);
+
+    }
+    public void printGroceryLit(String item){
+        grocerylist.add(item);
+
+    }
+}
+
+
+
+
+
+
+
