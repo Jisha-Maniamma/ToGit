f74f7fd6870bd02860c55bbf03cd6b2d8cbcce59
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-12-14 07:38:02 +0900

    14/12/2021

---
+++ src/com/Java/Class1/Day43/A2.java
@@ -0,0 +1,17 @@
+package com.Java.Class1.Day43;
+
+import com.Java.Class1.Day42.otaguroSan.test1.B;
+//import com.Java.Class1.Day42.*;
+
+public class A2 extends B {
+
+
+    public static void main(String[] args) {
+        A1 a=new A1();
+        A1.printHai();
+//        A3 b=new A3();
+//        B bb=new B();
+
+        System.out.println(Double.parseDouble("-1"));
+    }
+}
