162347fe61dbff652ddf5441c81d19829fc24df7
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-12-14 07:37:40 +0900

    14/12/2021

---
+++ src/com/Java/Class1/Day42/A.java
@@ -0,0 +1,27 @@
+package com.Java.Class1.Day42;
+
+public class A {
+
+    public int i=2;
+    public  int j=4;
+
+    public static int ii=3;
+    public static  int jj=6;
+
+
+    public int get_ii(){
+        return i;
+    }
+    public int get_jj(){
+        return j;
+    }
+
+
+}
+
+ class IntAA{
+    public int j11=2;
+}
+
+
+
