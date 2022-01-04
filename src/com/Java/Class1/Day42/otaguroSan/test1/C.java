bd650085221e07cfb24618519664c3c8187ca3b3
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-05 07:34:44 +0900

    complete the calculation 05/1/2022

---
+++ src/com/Java/Class1/Day42/otaguroSan/test1/C.java
@@ -0,0 +1,11 @@
+package com.Java.Class1.Day42.otaguroSan.test1;
+
+public class C {
+    public static void main(String[] args) {
+//        System.out.println(A.i);
+        System.out.println("..........................");
+        B bb=new B();
+//        bb.runn();
+//        System.out.println(A.i);
+    }
+}
