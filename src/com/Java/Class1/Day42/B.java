643f81df64538d6f37c8c33541ca7c3bd24b644b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-05 07:34:12 +0900

    complete the calculation 05/1/2022

---
+++ src/com/Java/Class1/Day42/B.java
@@ -0,0 +1,20 @@
+package com.Java.Class1.Day42;
+
+public class B {
+    public static void main(String[] args) {
+//        A test=new A();
+//
+//        System.out.println(test.i);
+//
+//       System.out.println(test.get_ii());
+//        System.out.println(test.get_jj());
+//
+//
+//        System.out.println(A.ii);
+//        System.out.println(A.jj);
+//
+//        IntAA aa=new IntAA();
+//        System.out.println(aa.j11);
+//        System.out.println(aa);
+    }
+}
