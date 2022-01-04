50d655fd0f6bc76dc67a6ffc732496605b0c9403
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-05 07:34:03 +0900

    complete the calculation 05/1/2022

---
+++ src/com/Java/Class1/Day42/otaguroSan/test1/B.java
@@ -0,0 +1,25 @@
+package com.Java.Class1.Day42.otaguroSan.test1;
+
+public class B {
+static String a;
+
+//    public  int runn(){
+//        A aa=new A();
+//        for(int ii=0;ii<10;ii++){
+//            aa.i++;
+//        }
+//       return aa.i;
+//    }
+
+
+    public static void main(String[] args) {
+        String b="";
+//        A aa=new A();
+       A.goFlag=false;
+
+        System.out.println(a+" ...."+b);
+//        System.out.println(aa.goFlag);
+    }
+
+
+}
