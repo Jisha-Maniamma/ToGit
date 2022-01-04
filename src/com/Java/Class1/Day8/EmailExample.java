23ae4e84b2e06aaff350149170cb46b279f84963
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-01-05 07:33:47 +0900

    complete the calculation 05/1/2022

--- src/com/Java/Class1/Day8/EmailExample.java
+++ src/com/Java/Class1/Day8/EmailExample.java
@@ -1,12 +1,12 @@
 package com.Java.Class1.Day8;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+//import javax.mail.Message;
+//import javax.mail.MessagingException;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.AddressException;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class EmailExample {
@@ -26,33 +26,33 @@ public class EmailExample {
         // Setup mail server
         properties.setProperty("mail.smtp.host", "10.101.3.229");
 
-        // Get the default Session object.
-        Session session = Session.getDefaultInstance(properties);
-
-
-        try{
-            // Create a default MimeMessage object.
-            MimeMessage message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-
-            // Now set the actual message
-            message.setText("This is actual message");
-
-            // Send message
-            Transport.send(message);
-            System.out.println("Sent message successfully....");
-        } catch (AddressException e) {
-            e.printStackTrace();
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+//        // Get the default Session object.
+//        Session session = Session.getDefaultInstance(properties);
+//
+//
+//        try{
+//            // Create a default MimeMessage object.
+//            MimeMessage message = new MimeMessage(session);
+//
+//            // Set From: header field of the header.
+//            message.setFrom(new InternetAddress(from));
+//
+//            // Set To: header field of the header.
+//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+//
+//            // Set Subject: header field
+//            message.setSubject("This is the Subject Line!");
+//
+//            // Now set the actual message
+//            message.setText("This is actual message");
+//
+//            // Send message
+//            Transport.send(message);
+//            System.out.println("Sent message successfully....");
+//        } catch (AddressException e) {
+//            e.printStackTrace();
+//        } catch (MessagingException e) {
+//            e.printStackTrace();
+//        }
     }
 }
