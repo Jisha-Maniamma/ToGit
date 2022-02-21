0f4d30a0e0c177868eacaf97492a3ba97648b80b
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-22 07:28:32 +0900

    examQuestion

--- src/com/Java/Class1/sde1.java
+++ src/com/Java/Class1/sde1.java
@@ -1,34 +1,42 @@
 package com.Java.Class1;
 
 public class sde1 {
+    public static StringBuilder out=new StringBuilder();
     public static int[] Totalcount=new int[26];
     public static void main(String[] args) {
         System.out.println("......................");
-        //System.out.println("hai".length());
-        getNumbers("abcd",1);
+
+        getNumbers("abcd",0);
+        ;
+        Long l=2777777777777777L;
+
+
         for(int i: Totalcount)
             System.out.print(i+" ");
+
+
+            System.out.println(out);
+
+        System.out.println("abcd".substring(0,0));
     }
 
 
     static public void getNumbers(String s,int num){
+        String[] ss=new String[200];
         //s='abcd' num=4
         int[] count= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-        if(num==1){
-            count[0]=s.length();
+        for (int i = 0; i < s.length() ; i++) {
+            out.append(s.charAt(i)+",");
+            System.out.println(i);
+            System.out.println(s.substring(0,i)+","+s.substring(i));
 
+
+            getNumbers(s.substring(0,i)+s.substring(i+1),i);
         }
-        else {
-            for (int i = 0; i < s.length() & i < num; i++) {
 
 
-
-
-            }
-
-
-        }
+count[0]=s.length();
         Totalcount=count;
 
     }
