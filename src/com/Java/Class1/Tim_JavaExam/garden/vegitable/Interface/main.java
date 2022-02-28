207eb3069b9f0ab549dea56f6a81362ef72acad1
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-02-28 11:45:10 +0900

    examQuestion

---
+++ src/com/Java/Class1/Tim_JavaExam/garden/vegitable/Interface/main.java
@@ -0,0 +1,17 @@
+package com.Java.Class1.Tim_JavaExam.garden.vegitable.Interface;
+
+public class main {
+    int x=0;
+
+    main(){
+       x++;
+    }
+    public static void main(String[] args) {
+        int x=20;
+       main m=new main();
+        System.out.println(m.x);
+    }
+}
+class people{
+
+}
