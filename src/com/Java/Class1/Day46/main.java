d92e9d9dbc015df472960b4431b9a390960a7083
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-03-08 13:34:08 +0900

    examQuestion

--- src/com/Java/Class1/Day46/main.java
+++ src/com/Java/Class1/Day46/main.java
@@ -1,15 +1,12 @@
 package com.Java.Class1.Day46;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class main {
     private static Map<String,celetialbodies> solarSystem=new HashMap<>();
     private static Set<celetialbodies> planets=new HashSet<>();
 
-
+    private static Set<celetialbodies> planets1=new TreeSet<>();
     public static void main(String[] args) {
         celetialbodies temp=new celetialbodies("Erth",365);
         solarSystem.put(temp.getName(), temp);
@@ -17,6 +14,7 @@ public class main {
         celetialbodies tempMoon = new celetialbodies("Moon", 27);
         temp.addMoon(tempMoon);
         solarSystem.put(tempMoon.getName(), tempMoon);
+
         temp = new celetialbodies("Mars", 687);
         solarSystem.put(temp.getName(), temp);
         planets.add(temp);
@@ -28,7 +26,20 @@ public class main {
         tempMoon = new celetialbodies("Phobos", 0.3);
         solarSystem.put(tempMoon.getName(), tempMoon);
         temp.addMoon(tempMoon); // temp is still Mars
+        for(celetialbodies a:planets){
+            System.out.println(a.getName()+" "+a.getOrbitalPeriod()+" "+a.gtSatellites());
+        }
 
+        for(celetialbodies a:solarSystem.get("Mars").gtSatellites()){
+            System.out.println(a.getName()+" "+a.getOrbitalPeriod()+" "+a.gtSatellites().size());
+        }
 
+        Set<celetialbodies> moons = new HashSet<>();
+        for(celetialbodies a:planets){
+          moons.addAll(a.gtSatellites());
+        }
+        for(celetialbodies moon : moons) {
+            System.out.println("\t" + moon.getName());
+        }
     }
 }
