21194bc5e7b69dbfc20805ffb7195f19ebb9e5ac
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-04-22 07:45:16 +0900

    reading writing

--- src/com/Java/Class1/Day12/Database1/DB/Main.java
+++ src/com/Java/Class1/Day12/Database1/DB/Main.java
@@ -1,7 +1,5 @@
 package com.Java.Class1.Day12.Database1.DB;
 
-import com.Java.Class1.Day12.Database1.DB.Model.DataSource;
-
 /**
  * @author Jisha Maniamma
  * @version 1.0
@@ -9,16 +7,5 @@ import com.Java.Class1.Day12.Database1.DB.Model.DataSource;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        DataSource dataSource=new DataSource();
-        if(!dataSource.open()){
-            System.out.println("!error cannot open");
-            return;
-        }
-
-        if(dataSource.createTable()){
-            System.out.println("Tables successfully created");
-        }
-        dataSource.close();
-    }
+   /***confidential****/
 }
