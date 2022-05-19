c4f7ceafe9200dbd75df22235cdfa0a1fef29770
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2022-05-19 15:01:02 +0900

    Sorting

--- src/com/Java/Class1/A00/man.java
+++ src/com/Java/Class1/A00/man.java
@@ -16,12 +16,12 @@ public class man {
         int count=0;
         String[] myArray = new String[ss.size()];
         System.out.println(ss.toArray(myArray));
-        //Arrays.asList name.toArray(ara)
+//        Arrays.asList name.toArray(ara)
         for(int i=0;i<ss.size();i++){
 count=1;
             for(int j=i+1;j<ss.size();j++){
                 if(ss.get(i).equals(ss.get(j))){
-                 //   System.out.println(ss.get(i));
+                    System.out.println(ss.get(i));
                   count=count+1;
                 //    System.out.println(ss.get(i)+" "+ss.get(j)+" "+count);
                 }
